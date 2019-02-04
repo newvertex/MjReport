7da63ffdeb1568bf532e1b8b26da95a3c7ee8d2c
Mojtaba Rabiei <mojtaba.r127@gmail.com>
2019-02-04 11:50:15 +0330

    add newPage to create new page and increase page number

--- src/Renderer.ts
+++ src/Renderer.ts
@@ -5,7 +5,7 @@ class Renderer {
   private pageNumber = 0;
 
   constructor(public data: MjData) {}
-
+  
   draw() {
     console.log('Renderer initializing . . . .');
     console.log('Loading meta data');
@@ -31,6 +31,34 @@ class Renderer {
       el.innerHTML = item.value || '';
       root.append(el);
     }
+  }
+
+  addHeaderFooter(page: HTMLElement) {
+    let header = this.createElement('div', 'header_section', `header_${this.pageNumber}`);
+    // header.innerHTML = this.data.header ;
+    page.append(header)
+    
+    let footer = this.createElement('div', 'footer_section', `footer_${this.pageNumber}`);
+    // footer.innerHTML = this.data.footer ;
+    page.append(footer)
+  }
+
+  // Create a new report page
+  private newPage(
+    root: HTMLElement,
+    paperType: PaperType = PaperType.A4_Portrait
+  ): HTMLElement {
+    this.pageNumber += 1; // Increase the page number
+
+    let page = this.createElement('page', 'page_section', `page_${this.pageNumber}`);
+    page.setAttribute('type', `${paperType}`);
+
+    // add header & footer to the page
+    this.addHeaderFooter(page);
+
+    root.append(page);
+
+    return page;
   }
 
   // Create a new html element
